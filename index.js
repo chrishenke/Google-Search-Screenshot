a134c89295f7b14436cba98b18384f407d68edd4
Chris Henke <chenke@cirrusabs.com>
2019-10-16 13:13:39 -0400

    Fixing geolocation issue

--- index.js
+++ index.js
@@ -1,51 +1,72 @@
 const puppeteer = require("puppeteer");
 const moment = require("moment");
 const cities = [
-    {
-        city:"San Angelo",
-        state:"TX",
-        country:"USA",
-        lat: 31.442778,
-        long: -100.450279
-    },
-    {
-        city:"Woodbridge",
-        state:"NJ",
-        country:"USA",
-        lat: 40.560001,
-        long:-74.290001
-    },
-    {
-        city:"Clinton",
-        state:"MI",
-        country:"USA",
-        lat: 42.586945,
-        long:-82.919998
-    }
-]
+	{
+		city: "San Angelo",
+		state: "TX",
+		country: "USA",
+		lat: 31.442778,
+		long: -100.450279
+	},
+	{
+		city: "Woodbridge",
+		state: "NJ",
+		country: "USA",
+		lat: 40.560001,
+		long: -74.290001
+	},
+	{
+		city: "Clinton",
+		state: "MI",
+		country: "USA",
+		lat: 42.586945,
+		long: -82.919998
+	},
+	{
+		city: "Fort Wayne",
+		state: "IN",
+		country: "USA",
+		lat: 41.079273,
+		long: -85.139351
+	}
+];
 
-async function getPic(url, query, city) {
-    const date = new Date();
-    const filepath = "screenshots/" + moment(date).format('YYYY-MM-DD') + '-' + query + "-" + city.city + ".png";
-    const browser = await puppeteer.launch();
+async function getScreenshot(url, query, city) {
+	const date = new Date();
+	const filepath =
+		"screenshots/" +
+		moment(date).format("YYYY-MM-DD") +
+		"-" +
+		query +
+		"-" +
+		city.city +
+		".png";
+	const browser = await puppeteer.launch({ headless: false, slowMo: 200 });
 
-    const urlObj = new URL(url);
+	// Grants permission for changing geolocation
+	const context = browser.defaultBrowserContext();
+	await context.overridePermissions("https://www.google.com", [
+		"geolocation"
+	]);
 
-    // Grants permission for changing geolocation
-    const context = browser.defaultBrowserContext();
-    context.clearPermissionOverrides();
-    await context.overridePermissions(urlObj.origin, ['geolocation']);
-    
-    const page = await context.newPage();
-    await page.setGeolocation({ latitude: city.lat, longitude: city.long }); 
- 	await page.goto(urlObj.href + "?q=" + query);
- 	const granted = await page.evaluate(async () => {
-        return (await navigator.permissions.query({name: 'geolocation'})).state;
-      });
-      console.log('Granted:', granted);
-  	await page.screenshot({ path: filepath.replace(" ","-"), fullPage: true });
- 	await browser.close();
+	const page = await context.newPage();
+
+	await page.setGeolocation({ latitude: city.lat, longitude: city.long });
+	await page.goto(url);
+
+	await page.focus('input[name = "q"]');
+	await page.keyboard.type(query);
+	await page.keyboard.press("Enter");
+	await page.waitForSelector("title");
+
+	await page.focus('input[name = "q"]');
+	await page.keyboard.press("Enter");
+	await page.waitForSelector("title");
+
+	await page.screenshot({ path: filepath.replace(" ", "-"), fullPage: true });
+	await browser.close();
 }
 
-//getPic('https://google.com​/search','pizza', );
-cities.forEach(city => getPic("https://google.com/search","pizza",city));
+cities.forEach(
+	async city => await getScreenshot("https://google.com", "pizza", city)
+);
